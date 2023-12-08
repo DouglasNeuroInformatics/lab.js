5ae2e91fcbf02780f26ebdb1df3b976a5260b763
joshunrau <contact@joshuaunrau.com>
2023-12-08 14:48:59 -0500

    add display name

--- packages/builder/src/components/ComponentOptions/components/Behavior/components/Meta.js
+++ packages/builder/src/components/ComponentOptions/components/Behavior/components/Meta.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
 import { Field, useFormikContext } from 'formik'
-import { FormGroup, Col, Label,
-  InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap'
+import {
+  FormGroup,
+  Col,
+  Label,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+} from 'reactstrap'
 
 import Card from '../../../../Card'
 import Hint from '../../../../Hint'
 import { Input, CustomInput } from '../../../../Form'
 
-export default () => {
+const Meta = () => {
   const { values } = useFormikContext()
 
   return (
@@ -16,10 +22,7 @@ export default () => {
       <FormGroup row>
         <Label for="skip" xs="2">
           Skip
-          <Hint
-            title="Skip"
-            className="float-right"
-          >
+          <Hint title="Skip" className="float-right">
             <p className="font-weight-bold">
               Don't run the component during the study.
             </p>
@@ -33,19 +36,20 @@ export default () => {
             <InputGroupAddon addonType="prepend">
               <InputGroupText>
                 <Field
-                  name="skip" id="skip"
-                  component={ CustomInput }
+                  name="skip"
+                  id="skip"
+                  component={CustomInput}
                   type="checkbox"
                 />
               </InputGroupText>
             </InputGroupAddon>
             <Field
               name="skipCondition"
-              component={ Input }
+              component={Input}
               className="text-monospace"
               // eslint-disable-next-line no-template-curly-in-string
               placeholder="${ optional condition }"
-              disabled={ values.skip === true }
+              disabled={values.skip === true}
             />
           </InputGroup>
         </Col>
@@ -53,15 +57,15 @@ export default () => {
       <FormGroup row>
         <Label for="tardy" xs="2">
           Tardy
-          <Hint
-            title="Tardiness"
-            className="float-right"
-          >
+          <Hint title="Tardiness" className="float-right">
             <p className="font-weight-bold">
-              Prepare the component at the last possible moment, just before it is run.
+              Prepare the component at the last possible moment, just before it
+              is run.
             </p>
             <p className="text-muted">
-              This is useful if the component depends on information that becomes available only during the study, for example if a screen's content depends on participant behavior.
+              This is useful if the component depends on information that
+              becomes available only during the study, for example if a screen's
+              content depends on participant behavior.
             </p>
             <p className="text-muted">
               Ordinarily, a component is prepared when the page is loaded.
@@ -70,8 +74,9 @@ export default () => {
         </Label>
         <Col xs="10" className="pt-2">
           <Field
-            name="tardy" id="tardy"
-            component={ CustomInput }
+            name="tardy"
+            id="tardy"
+            component={CustomInput}
             type="checkbox"
           />
         </Col>
@@ -79,3 +84,5 @@ export default () => {
     </Card>
   )
 }
+
+export default Meta
