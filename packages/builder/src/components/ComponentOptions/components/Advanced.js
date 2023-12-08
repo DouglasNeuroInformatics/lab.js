de32a0117721e230da4e41c86f31ae4c3bfc2f0b
joshunrau <contact@joshuaunrau.com>
2023-12-08 14:48:37 -0500

    add display name

--- packages/builder/src/components/ComponentOptions/components/Advanced.js
+++ packages/builder/src/components/ComponentOptions/components/Advanced.js
@@ -9,23 +9,18 @@ import Hint from '../../Hint'
 import Form from './Form'
 import { CustomInput } from '../../Form'
 
-export default ({ id, data }) =>
+const Advanced = ({ id, data }) => (
   <Card title="Advanced options">
-    <Form
-      id={ id } data={ data }
-      keys={ ['scrollTop', '_template'] }
-    >
+    <Form id={id} data={data} keys={['scrollTop', '_template']}>
       <FormGroup row>
         <Col xs={2}>
-          <Label>
-            Run
-          </Label>
+          <Label>Run</Label>
         </Col>
         <Col xs={10}>
           <FormGroup check>
             <Field
               name="scrollTop"
-              component={ CustomInput }
+              component={CustomInput}
               id="scrollTop"
               type="checkbox"
               inline={true}
@@ -38,14 +33,13 @@ export default ({ id, data }) =>
               }}
             >
               <p className="font-weight-bold">
-                Reset scroll position to the top of the page
-                when showing content
+                Reset scroll position to the top of the page when showing
+                content
               </p>
               <p className="text-muted">
-                This is useful if the preceding and current screen
-                both occupy a lot of vertical space, as it saves
-                participants from having to scroll back upwards
-                manually between pages.
+                This is useful if the preceding and current screen both occupy a
+                lot of vertical space, as it saves participants from having to
+                scroll back upwards manually between pages.
               </p>
             </Hint>
           </FormGroup>
@@ -53,15 +47,13 @@ export default ({ id, data }) =>
       </FormGroup>
       <FormGroup row>
         <Col xs={2}>
-          <Label>
-            Display
-          </Label>
+          <Label>Display</Label>
         </Col>
         <Col xs={10}>
           <FormGroup check>
             <Field
               name="_template"
-              component={ CustomInput }
+              component={CustomInput}
               id="_template"
               type="checkbox"
               inline={true}
@@ -77,15 +69,20 @@ export default ({ id, data }) =>
                 Reduce visible settings so that only parameters can be changed.
               </p>
               <p className="text-muted">
-                Making a component a template simplifies complex components and bundle nested hierarchies into a single module. This creates reusable components that are defined only by their parameters.
+                Making a component a template simplifies complex components and
+                bundle nested hierarchies into a single module. This creates
+                reusable components that are defined only by their parameters.
               </p>
               <p className="text-muted">
-                Making a component a template prevents direct modification of all component details, and hides nested components.
+                Making a component a template prevents direct modification of
+                all component details, and hides nested components.
               </p>
             </Hint>
           </FormGroup>
         </Col>
       </FormGroup>
-
     </Form>
   </Card>
+)
+
+export default Advanced
