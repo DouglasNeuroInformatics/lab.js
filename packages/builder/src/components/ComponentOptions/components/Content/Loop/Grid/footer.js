7cc8d62910fa3828bdc6e28cbfbafaa7a9cd3725
RyanHaniff <haniffryan.hr@gmail.com>
2023-12-12 12:31:43 -0500

    Format and rename Grid footer component

--- packages/builder/src/components/ComponentOptions/components/Content/Loop/Grid/footer.js
+++ packages/builder/src/components/ComponentOptions/components/Content/Loop/Grid/footer.js
@@ -1,12 +1,17 @@
 import React, { useState, createRef } from 'react'
 
-import { ButtonDropdown, Button,
-  DropdownToggle, DropdownMenu, DropdownItem  } from 'reactstrap'
+import {
+  ButtonDropdown,
+  Button,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
 import { range } from 'lodash'
 
 import Icon from '../../../../../Icon'
 import FactorialModal from '../DesignWizard/FactorialModal'
-import { useArrayContext } from '../../../../../Form/array'
+import { useArrayContext } from '@/hooks/useArrayContext'
 
 import Uploader from '../../../../../Uploader'
 import { saveAs } from 'file-saver'
@@ -14,20 +19,17 @@ import { parse, unparse } from 'papaparse'
 
 const exportGrid = (data, columns) => {
   const output = unparse({
-    fields: columns.map(c => c.name),
-    data
+    fields: columns.map((c) => c.name),
+    data,
   })
 
   return saveAs(
-    new Blob(
-      [ output ],
-      { type: 'text/csv;charset=utf-8' }
-    ),
-    'loop.csv'
+    new Blob([output], { type: 'text/csv;charset=utf-8' }),
+    'loop.csv',
   )
 }
 
-export default ({ addItem, columns }) => {
+const GridFooter = ({ addItem, columns }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false)
   const factorialModal = createRef()
   const { dispatch, overwriteAll } = useArrayContext()
@@ -36,20 +38,22 @@ export default ({ addItem, columns }) => {
     <tfoot>
       <tr>
         <td />
-        <td colSpan={ columns }>
-          <FactorialModal ref={ factorialModal } />
+        <td colSpan={columns}>
+          <FactorialModal ref={factorialModal} />
           <ButtonDropdown
             size="sm"
             className="w-100"
-            isOpen={ dropdownOpen }
-            toggle={ () => setDropdownOpen(!dropdownOpen) }
+            isOpen={dropdownOpen}
+            toggle={() => setDropdownOpen(!dropdownOpen)}
           >
             <Button
-              block size="sm"
-              outline color="muted"
+              block
+              size="sm"
+              outline
+              color="muted"
               className="hover-target"
-              onClick={ () => addItem(Array(columns).fill('')) }
-              onMouseUp={ e => e.target.blur() }
+              onClick={() => addItem(Array(columns).fill(''))}
+              onMouseUp={(e) => e.target.blur()}
               style={{
                 paddingLeft: '32px', // 6px standard + 24px toggle width
               }}
@@ -57,8 +61,11 @@ export default ({ addItem, columns }) => {
               <Icon icon="plus" />
             </Button>
             <DropdownToggle
-              caret split size="sm"
-              outline color="muted"
+              caret
+              split
+              size="sm"
+              outline
+              color="muted"
               className="hover-target"
               style={{
                 width: '24px',
@@ -67,29 +74,31 @@ export default ({ addItem, columns }) => {
             <DropdownMenu right>
               <DropdownItem header>Generate</DropdownItem>
               <DropdownItem
-                onClick={ () => factorialModal.current.show().then(result => {
-                  overwriteAll(result.rows, result.columns)
-                }) }
+                onClick={() =>
+                  factorialModal.current.show().then((result) => {
+                    overwriteAll(result.rows, result.columns)
+                  })
+                }
               >
                 Factorial design
               </DropdownItem>
               <DropdownItem divider />
               <DropdownItem header>Repeat</DropdownItem>
               <DropdownItem
-                onClick={ () => {
+                onClick={() => {
                   const n = window.prompt('How many times?')
                   if (n) {
                     dispatch((rows, columns) => [
-                      rows.flatMap(r => range(n).map(() => r)),
+                      rows.flatMap((r) => range(n).map(() => r)),
                       columns,
                     ])
                   }
-                } }
+                }}
               >
                 Each row
               </DropdownItem>
               <DropdownItem
-                onClick={ () => {
+                onClick={() => {
                   const n = window.prompt('How many times?')
                   if (n) {
                     dispatch((rows, columns) => [
@@ -97,7 +106,7 @@ export default ({ addItem, columns }) => {
                       columns,
                     ])
                   }
-                } }
+                }}
               >
                 Entire grid
               </DropdownItem>
@@ -105,56 +114,51 @@ export default ({ addItem, columns }) => {
               <DropdownItem header>CSV / TSV</DropdownItem>
               <Uploader
                 accept="text/csv,text/tab-separated-values,.csv,.tsv"
-                multiple={ false }
-                onUpload={
-                  ([[content]]) => {
-                    // TODO: Close the dropdown when the file selector
-                    // is shown. This needs some more work to make sure
-                    // that the uploader component isn't removed from the
-                    // page in between
-                    setDropdownOpen(false)
+                multiple={false}
+                onUpload={([[content]]) => {
+                  // TODO: Close the dropdown when the file selector
+                  // is shown. This needs some more work to make sure
+                  // that the uploader component isn't removed from the
+                  // page in between
+                  setDropdownOpen(false)
 
-                    const parseResult = parse(
-                      content.trim(),
-                      { header: true }
+                  const parseResult = parse(content.trim(), { header: true })
+
+                  if (
+                    // No detected issues
+                    parseResult.errors.length === 0 ||
+                    // Data is present, but no delimiter found
+                    (parseResult.data &&
+                      parseResult.errors.length === 1 &&
+                      parseResult.errors[0].code === 'UndetectableDelimiter')
+                  ) {
+                    overwriteAll(
+                      parseResult.data.map((r) => Object.values(r)),
+                      Object.keys(parseResult.data[0]).map((c) => ({
+                        name: c,
+                        type: 'string',
+                      })),
                     )
-
-                    if (
-                      // No detected issues
-                      parseResult.errors.length === 0 ||
-                      // Data is present, but no delimiter found
-                      (parseResult.data && parseResult.errors.length === 1 &&
-                        parseResult.errors[0].code === 'UndetectableDelimiter')
-                    ) {
-                      overwriteAll(
-                        parseResult.data.map(r => Object.values(r)),
-                        Object.keys(parseResult.data[0])
-                          .map(c => ({ name: c, type: 'string' })),
-                      )
-                    } else {
-                      console.log(
-                        'CSV import found errors: ',
-                        parseResult.errors
-                      )
-                      alert(
-                        'Sorry, I couldn\'t parse that CSV file. ' +
+                  } else {
+                    console.log('CSV import found errors: ', parseResult.errors)
+                    alert(
+                      "Sorry, I couldn't parse that CSV file. " +
                         'There seems to be an error. ' +
-                        'Could you check it, please?'
-                      )
-                    }
+                        'Could you check it, please?',
+                    )
                   }
-                }
+                }}
               >
-                <div className="dropdown-item">
-                  Import
-                </div>
+                <div className="dropdown-item">Import</div>
               </Uploader>
               <DropdownItem
-                onClick={ () => dispatch((rows, columns) => {
-                  exportGrid(rows, columns)
-                  // TODO: Can we do without rewriting the data?
-                  return [rows, columns]
-                }) }
+                onClick={() =>
+                  dispatch((rows, columns) => {
+                    exportGrid(rows, columns)
+                    // TODO: Can we do without rewriting the data?
+                    return [rows, columns]
+                  })
+                }
               >
                 Export
               </DropdownItem>
@@ -166,3 +170,5 @@ export default ({ addItem, columns }) => {
     </tfoot>
   )
 }
+
+export default GridFooter
